c1fd902b62bef069e3c7e9005f0f6ebd69cd7bb3
Felix <fedecraft3312@gmail.com>
2024-05-11 20:33:31 -0300

    Completed tutorial

--- src/index.ts
+++ src/index.ts
@@ -3,90 +3,110 @@ import { createServer } from "node:http";
 import { join } from "node:path";
 import sqlite3 from "sqlite3";
 import { open } from "sqlite";
-
-// This one is not working for me I don't know why
 import { Server } from "socket.io";
+import { availableParallelism } from "node:os";
+import cluster from "node:cluster";
+import { createAdapter, setupPrimary } from "@socket.io/cluster-adapter";
 
 export const CWD = process.cwd();
 
-const app = express();
-const server = createServer(app);
-const io = new Server(server, {
-    connectionStateRecovery: {},
-});
+const PORT = 37000;
 
-(async () => {
-    // Connect to sqlite and create messages table
-    const db = await open({
-        filename: "chat.db",
-        driver: sqlite3.Database,
-    });
+if(cluster.isPrimary) {
+    const numCpus = availableParallelism();
     
-    // Create messages table
-    await db.exec(`
-        CREATE TABLE IF NOT EXISTS messages (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            client_offset TEXT UNIQUE,
-            content TEXT
-        )
-    `);
+    // Create one worker per available core
+    for(let i = 0; i < numCpus; i++) {
+        cluster.fork({
+            PORT: PORT + i,
+        });
+    }
     
-    app.get('/', (req, res) => {
-        return res.sendFile(join(CWD, "index.html"));
-    });
-    
-    // Listen for connections
-    io.on('connection', async (socket) => {
-        console.log(`A user connected`);
+    // Set up the adapter on the primary thread
+    setupPrimary();
+} else {
+    const app = express();
+    const server = createServer(app);
+    const io = new Server(server, {
+        connectionStateRecovery: {},
         
-        socket.on("disconnect", () => {
-            console.log(`User disconnected`);
+        // Set up the adapter on each worker thread
+        adapter: createAdapter(),
+    });
+
+    (async () => {
+        // Connect to sqlite and create messages table
+        const db = await open({
+            filename: "chat.db",
+            driver: sqlite3.Database,
         });
         
-        socket.on("chat message", async (msg, clientOffset, callback) => {
-            let result;
-            try {
-                // Store messages in the database
-                result = await db.run(`INSERT INTO messages (content, client_offset) VALUES (?, ?)`, msg, clientOffset);
-            } catch(err: any) {
-                console.error(err);
-                if(err.errno === 19) {
-                    // The message was already inserted so we notify the client
-                    callback();
-                } else {
-                    // Nothing to do, just let the client retry
-                }
-                return;
-            }
-            
-            io.emit("chat message", msg, result.lastID);
-            
-            // Acknowledge the callback
-            callback();
+        // Create messages table
+        await db.exec(`
+            CREATE TABLE IF NOT EXISTS messages (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                client_offset TEXT UNIQUE,
+                content TEXT
+            )
+        `);
+        
+        app.get('/', (req, res) => {
+            return res.sendFile(join(CWD, "index.html"));
         });
         
-        if(!socket.recovered) {
-            try {
-                await db.each(
-                    "SELECT id, content FROM messages WHERE id > ?",
-                    [socket.handshake.auth.serverOffset || 0],
-                    (_err, row) => {
-                        socket.emit("chat message", row.content, row.id);
+        // Listen for connections
+        io.on('connection', async (socket) => {
+            console.log(`A user connected`);
+            
+            socket.on("disconnect", () => {
+                console.log(`User disconnected`);
+            });
+            
+            socket.on("chat message", async (msg, clientOffset, callback) => {
+                let result;
+                try {
+                    // Store messages in the database
+                    result = await db.run(`INSERT INTO messages (content, client_offset) VALUES (?, ?)`, msg, clientOffset);
+                } catch(err: any) {
+                    console.error(err);
+                    if(err.errno === 19) {
+                        // The message was already inserted so we notify the client
+                        callback();
+                    } else {
+                        // Nothing to do, just let the client retry
                     }
-                )
-            } catch(err: any) {
-                console.error(err);
+                    return;
+                }
+                
+                io.emit("chat message", msg, result.lastID);
+                
+                // Acknowledge the callback
+                callback();
+            });
+            
+            if(!socket.recovered) {
+                try {
+                    await db.each(
+                        "SELECT id, content FROM messages WHERE id > ?",
+                        [socket.handshake.auth.serverOffset || 0],
+                        (_err, row) => {
+                            socket.emit("chat message", row.content, row.id);
+                        }
+                    )
+                } catch(err: any) {
+                    console.error(err);
+                }
             }
-        }
+            
+            socket.broadcast.emit("hi");
+        });
         
-        socket.broadcast.emit("hi");
-    });
-    
-    io.emit("hello", "world");
-    
-    // Start server
-    const PORT = 37000;
-    server.listen(PORT, () => {
-        console.log(`Server listening on http://localhost:${PORT}`);
-    });
-})();
+        io.emit("hello", "world");
+        
+        // Start server
+        const port = process.env.PORT;
+        server.listen(port, () => {
+            console.log(`Server listening on http://localhost:${port}`);
+        });
+    })();
+}
